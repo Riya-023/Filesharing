326a7512757746b785e40cdb8a0365f506ec6f56
Riya <riyabhatia230@gmail.com>
2024-12-29 19:03:44 +0530

    First commit

---
+++ src/main/java/com/example/controller/filecontroller.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping()
+public class filecontroller {
+
+    @GetMapping("/files")
+    public String login() {
+        return "home";
+    }
+
+    @GetMapping("/list")
+    public String files() {
+        return "list-files";
+    }
+
+    @GetMapping("/share")
+    public String share() {
+        return "share-file";
+    }
+}
